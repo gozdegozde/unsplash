3f076b5463f2c198ab008e36888b8c6d9d0140e0
gozde <gozdeoztoprak0@gmail.com>
2022-02-12 19:22:53 +0100

    add style attribute in main page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Images from './pages/Images';
 function App() {
   return (
     <div >
-      <h1>Images</h1>
+      <h1 style={{display: 'flex',  justifyContent:'center', alignItems:'center',fontSize:'160px'}}>Images</h1>
       <Images/>
     </div>
   );
